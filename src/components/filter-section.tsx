40a2fdb16c760399a50e35fc67dd0a5d59163099
Andriu Garcia <contacto@andriugarcia.com>
2025-05-26 21:35:27 +0200

    style: filter menus

--- src/components/filter-section.tsx
+++ src/components/filter-section.tsx
@@ -11,36 +11,36 @@ import { X, ChevronRight, ChevronDown } from "lucide-react";
 import { useState } from "react";
 
 type FilterSectionProps = {
-    type: string;
-    filters: Filter[];
-    onFilterSelected: (filter: Filter) => void;
+  type: string;
+  filters: Filter[];
+  onFilterSelected: (filter: Filter) => void;
 }
 
-export function FilterSection({type, filters, defaultOpen, onFilterSelected, selectedFilters}: FilterSectionProps) {
-    const [opened, setOpen] = useState(defaultOpen);
-    const renderBadge = (filter: Filter | string, dismissible: boolean = false) => {
-      if (typeof filter === 'string') {
-        filter = (filtersData as Record<string, Filter>)[filter] ?? { name: filter, type: "others", color: "gray" };
-      }
-      
-      return (<Badge color={filter.color} onClick={() => onFilterSelected(filter)}>{filter.name} {dismissible ? <X></X> : null}</Badge>)
+export function FilterSection({ type, filters, defaultOpen, onFilterSelected, selectedFilters }: FilterSectionProps) {
+  const [opened, setOpen] = useState(defaultOpen);
+  const renderBadge = (filter: Filter | string, dismissible: boolean = false) => {
+    if (typeof filter === 'string') {
+      filter = (filtersData as Record<string, Filter>)[filter] ?? { name: filter, type: "others", color: "gray" };
     }
 
-    return (
-        <Collapsible className="mb-2" defaultOpen={opened} key={type} onOpenChange={setOpen}>
-        <CollapsibleTrigger className="flex items-center gap-2">
+    return (<Badge color={filter.color} onClick={() => onFilterSelected(filter)}>{filter.name} {dismissible ? <X></X> : null}</Badge>)
+  }
+
+  return (
+    <Collapsible className="mb-2" defaultOpen={opened} key={type} onOpenChange={setOpen}>
+      <CollapsibleTrigger className="flex items-center p-1 gap-2 w-full hover:bg-white/10 rounded-lg">
+        {
+          opened ? <ChevronDown size={18}></ChevronDown> : <ChevronRight size={18}></ChevronRight>
+        }
+        <div className="leading-7">{type}</div>
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <div className="flex flex-wrap gap-2 my-2 ml-6">
           {
-            opened ? <ChevronDown size={18}></ChevronDown> : <ChevronRight size={18}></ChevronRight>
+            filters?.map((filter) => renderBadge(filter, selectedFilters.includes(filter)))
           }
-          <div className="leading-7">{type}</div>
-        </CollapsibleTrigger>
-        <CollapsibleContent>
-          <div className="flex flex-wrap gap-2 my-2 ml-6">
-              {
-                  filters?.map((filter) => renderBadge(filter, selectedFilters.includes(filter)))
-              }
-          </div>
-        </CollapsibleContent>
-      </Collapsible>
-    )
+        </div>
+      </CollapsibleContent>
+    </Collapsible>
+  )
 }
