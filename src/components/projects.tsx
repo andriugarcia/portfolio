d51e9729d92702283e0b2234011a8d8e79ba3246
andriugarcia <GVDrews@gmail.com>
2025-06-16 21:57:10 +0200

    feat: make ui glass

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -23,19 +23,11 @@ import { FilterBar } from "./filter-bar";
 export function Projects() {
   const [selectedFilters, setSelectedFilters] = useState<Filter[]>([]);
   const [filterMobileOpened, setFilterMobileOpened] = useState(false);
-  const filtersPerType: { [type: string]: Filter[] } = Object.values(filtersData).reduce((acc: { [type: string]: Filter[] }, filter: Filter) => {
-    if (!acc[filter.type]) {
-      acc[filter.type] = [];
-    }
-    acc[filter.type].push(filter);
-    return acc;
-  }, {});
   const { filteredExperience, setKeywords } = useExperienceSearch();
 
   useEffect(() => {
     const url = new URL(window.location.href);
     const filterParams = url.searchParams.getAll('f');
-    console.log(filterParams);
     
     filterParams.forEach((filterParam) => {
       addFilter(filterParam);
@@ -90,14 +82,6 @@ export function Projects() {
     }
   }
 
-  const renderBadge = (filter: Filter | string, dismissible: boolean = false) => {
-    if (typeof filter === 'string') {
-      filter = (filtersData as Record<string, Filter>)[filter] ?? { name: filter, type: "others", color: "gray" };
-    }
-    
-    return (<Badge color={filter.color} onClick={() => addFilter(filter)}>{filter.name} {dismissible ? <X></X> : null}</Badge>)
-  }
-
   const clearFilters = () => {
     setSelectedFilters([]);
     setKeywords([]);
@@ -106,15 +90,15 @@ export function Projects() {
   const ProjectList = () => (<>
   {
     filteredExperience.map((experience) => (
-        <div className="mb-8" key={experience.company}>
-            <div className="text-2xl text-wrap">{experience.company}</div>
-            <div className="text-muted-foreground mb-4">{experience.team}</div>
+        <div key={experience.company} className="mt-5" key={experience.company}>
+            <div className="ml-4 text-2xl text-wrap">{experience.company}</div>
+            <div className="ml-4 text-muted-foreground mb-4">{experience.team}</div>
             <ul className="list-disc list-inside">
                 {
                     experience.highlights.map((highlight) => (
-                        <li className="text-pretty">{highlight.content} {
+                        <li key={highlight.content} className="text-pretty">{highlight.content} {
                           highlight.matchedKeywords.map((kw) => (
-                            <Badge variant="outline" className="text-xs">{kw}</Badge>
+                            <Badge key={kw} variant="outline" className="text-xs">{kw}</Badge>
                           ))
                         }</li>
                     ))
@@ -122,7 +106,7 @@ export function Projects() {
             </ul>
             {
                 experience.projects.map((project) => (
-                    <Card className="relative px-4 mb-4" key={project.name}>
+                    <Card className="relative px-4 mb-2" key={project.name}>
                         <div className="flex items-center justify-between">
                           <div className="font-semibold">{ project.name } </div>
                           <Button>
@@ -133,7 +117,7 @@ export function Projects() {
                         <div className="flex flex-wrap gap-2">
                             {
                                 project.stack.map((tech) => (
-                                    <Badge variant={selectedFilters.find(filter => filter.name === tech) ? 'default' : 'outline'}>{tech}</Badge>
+                                    <Badge key={tech} variant={selectedFilters.find(filter => filter.name === tech) ? 'default' : 'outline'}>{tech}</Badge>
                                 ))
                             }
                         </div>
@@ -146,9 +130,9 @@ export function Projects() {
                           <ul className="list-disc list-inside">
                               {
                                   project.highlights.map((highlight) => (
-                                      <li className="text-pretty text-purple-50">{highlight.content} {
+                                      <li key={highlight.content} className="text-pretty text-purple-50">{highlight.content} {
                                         highlight.matchedKeywords.map((kw) => (
-                                          <Badge variant="outline" className="text-xs">{kw}</Badge>
+                                          <Badge key={kw} variant="outline" className="text-xs">{kw}</Badge>
                                         ))
                                       }</li>
                                   ))
@@ -190,7 +174,7 @@ export function Projects() {
                             <div className="grid gap-2">
                               {project.images.map((src, index) => (
                                 index % 2 === 0 ? (
-                                <Card className="py-0">
+                                <Card key={src} className="py-0">
                                   <img className="rounded-xl" src={src} alt="" />
                                 </Card>) : null
                               ))}
@@ -198,7 +182,7 @@ export function Projects() {
                             <div className="grid gap-2">
                               {project.images.map((src, index) => (
                                 index % 2 !== 0 ? (
-                                  <Card className="py-0">
+                                  <Card key={src} className="py-0 h-fit">
                                     <img className="rounded-xl" src={src} alt="" />
                                   </Card>) : null
                               ))}
@@ -215,24 +199,27 @@ export function Projects() {
   </>);
 
   return (
-    <div className="flex flex-row h-full flex-1 min-h-0">
-        <div className="basis-[360px] pt-4 px-4 border-r-1 border-grey h-full flex flex-col hidden md:block">
+    <div className="flex flex-row h-full flex-1 gap-x-2 min-h-0 pb-4">
+        <Card className="basis-[360px] pt-4 px-4 border-r-1 border-grey h-full flex flex-col hidden md:block">
             <h2 className="text-2xl font-bold tracking-tight">Stack</h2>
             <p className="text-muted-foreground text-sm mb-2">Choose filters to showcase the enhanced experience</p>
             <ScrollArea className="w-full h-full">
               <FilterSelector selectedFilters={selectedFilters} addFilter={addFilter}></FilterSelector>
             </ScrollArea>
-        </div>
-        <div className="basis-auto md:basis-full pt-4 md:px-4 flex flex-col min-h-0">
-            <div className="flex items-center justify-between px-4">
-              <div>
-                <h2 className="text-2xl font-bold tracking-tight">Projects</h2>
-                <p className="text-muted-foreground text-sm mb-2">Projects I have worked on as a frontend developer</p>
+        </Card>
+        <div className="basis-auto md:basis-full flex flex-col min-h-0">
+            <Card className="py-4 gap-2 hidden md:block md:z-2">
+              <div className="flex items-center justify-between px-4">
+                <div>
+                  <h2 className="text-2xl font-bold tracking-tight">Projects</h2>
+                  <p className="text-muted-foreground text-sm mb-2">Projects I have worked on as a frontend developer</p>
+                </div>
+                <Button onClick={() => generateResumePDF(filteredExperience)}><div className="hidden md:block">Generate Resume </div><FileDown></FileDown></Button>
               </div>
-              <Button onClick={() => generateResumePDF(filteredExperience)}><div className="hidden md:block">Generate Resume </div><FileDown></FileDown></Button>
-            </div>
-            <FilterBar selectedFilters={selectedFilters} onFilterClick={addFilter} onClear={clearFilters} onFilterMobileOpened={() => setFilterMobileOpened(prev => !prev)}/>
-            <ScrollArea className="w-full flex-1 min-h-0 px-4">
+              <FilterBar selectedFilters={selectedFilters} onFilterClick={addFilter} onClear={clearFilters} onFilterMobileOpened={() => setFilterMobileOpened(prev => !prev)}/>
+            </Card>
+            <FilterBar className="md:hidden mx-0" selectedFilters={selectedFilters} onFilterClick={addFilter} onClear={clearFilters} onFilterMobileOpened={() => setFilterMobileOpened(prev => !prev)}/>
+            <ScrollArea className="w-full flex-1 min-h-0 md:-mt-2">
               {
                 filterMobileOpened ? (<FilterSelector selectedFilters={selectedFilters} addFilter={addFilter} onClose={() => setFilterMobileOpened(false)}></FilterSelector>)
                 : (<ProjectList></ProjectList>)
